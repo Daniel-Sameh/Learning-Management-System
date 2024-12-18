7dc46a1d6188dfa82c35b273ee5517c77ce57548
Danial Raafat <123320834+DannyRaafat@users.noreply.github.com>
2024-12-19 01:43:23 +0200

    Add files via upload

---
+++ src/main/java/com/swe/lms/courseManagement/dto/CourseWithInstructorDTO.java
@@ -0,0 +1,19 @@
+//package com.swe.lms.courseManagement.dto;
+//
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//public class CourseWithInstructorDTO {
+//    private String name;
+//    private String code;
+//    private String instructor;
+//
+//    public CourseWithInstructorDTO( String name, String code, String instructor) {
+//        this.name = name;
+//        this.code = code;
+//        this.instructor = instructor;
+//    }
+//
+//}
