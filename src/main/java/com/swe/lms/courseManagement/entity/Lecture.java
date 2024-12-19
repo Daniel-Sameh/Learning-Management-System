b4f962bb8e2b03bf2ae4f2498d5ba42fa080339b
Danial Raafat <123320834+DannyRaafat@users.noreply.github.com>
2024-12-19 15:29:51 +0200

    Update Lecture.java

--- src/main/java/com/swe/lms/courseManagement/entity/Lecture.java
+++ src/main/java/com/swe/lms/courseManagement/entity/Lecture.java
@@ -6,37 +6,68 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
+
 @Table(name="Lectures")
 public class Lecture {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "Date", nullable = false)
-    private LocalDateTime lectureDate;
-
     @OneToMany
     private List<User> attendanceList;
 
-    @Column(name="Course id")
-    private Long course_id;
-
     @Column(name="OTP")
     private String OTP;
+
+    @Column (name="Running")
+    private boolean running;
 
     @ManyToOne
     @JoinColumn(name = "course_id")
     private Course course;
 
+
+
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public List<User> getAttendanceList() {
+//        return attendanceList;
+//    }
+//
+//    public Long getCourse_id() {
+//        return course_id;
+//    }
+//
+//    public String getOTP() {
+//        return OTP;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public void setAttendanceList(List<User> attendanceList) {
+//        this.attendanceList = attendanceList;
+//    }
+//
+//    public void setCourse_id(Long course_id) {
+//        this.course_id = course_id;
+//    }
+//
+//    public void setOTP(String OTP) {
+//        this.OTP = OTP;
+//    }
+//
+//
 //    public void applyOTP(User user, String OTP){
 //        if(OTP.equals(this.OTP) && user.getRole().equals("Student") && !attendanceList.contains(user)){
 //            attendanceList.add(user);
