e669e36617bcecd1c9b167552f43bd3f79a72b40
Alaa Ashraf <125699791+alaaashraf19@users.noreply.github.com>
2024-12-22 17:29:31 +0200

    added get and delete mapping to questionbank.java

--- src/main/java/com/swe/lms/AssessmentManagement/Controller/QuestionBankController.java
+++ src/main/java/com/swe/lms/AssessmentManagement/Controller/QuestionBankController.java
@@ -1,7 +1,11 @@
 package com.swe.lms.AssessmentManagement.Controller;
 
+import com.swe.lms.AssessmentManagement.Mapper.QuestionMapper;
+import com.swe.lms.AssessmentManagement.Repository.QuestionRepository;
 import com.swe.lms.AssessmentManagement.Service.QuestionBankService;
 import com.swe.lms.AssessmentManagement.Service.QuestionRequest;
+import com.swe.lms.AssessmentManagement.dto.QuestionDto;
+import com.swe.lms.AssessmentManagement.entity.QuestionBank;
 import com.swe.lms.AssessmentManagement.entity.Questions.Question;
 import com.swe.lms.courseManagement.Repository.CourseRepository;
 import com.swe.lms.courseManagement.entity.Course;
@@ -22,28 +26,67 @@ public class QuestionBankController {
     private QuestionBankService questionBankService;
     @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+
+
     @PostMapping("/create")
     @PreAuthorize("hasRole('ROLE_INSTRUCTOR')")
     public ResponseEntity<?> createBank(@AuthenticationPrincipal User instructor, @RequestBody QuestionBankRequest questionBankRequest) {
         Optional<Course> course=courseRepository.findById(questionBankRequest.getCourseid());
-        if (course.isEmpty()) { // not sure
+        if (course.isEmpty()) {
             return ResponseEntity.status(404).body("Course not found");
         }
         questionBankService.createBankTOCourse(questionBankRequest.getQuestions(),course.get());
         return ResponseEntity.ok("Question bank Created");
-
     }
 
-//    @PreAuthorize("hasRole('ROLE_INSTRUCTOR')")
-//    @PostMapping("/add")
-//    public void addQuestion(@RequestBody Question question) {
-//        questionBankService.addQuestion(question);
-//    }
-//
-//    @PreAuthorize("hasRole('ROLE_INSTRUCTOR')")
-//    @GetMapping("/getquestions")
-//    public List<Question> getQuestions() {
-//        return questionBankService.getQuestions();
-//    }
+    @PostMapping("/{courseId}/addQuestion")
+    @PreAuthorize("hasRole('ROLE_INSTRUCTOR')")
+    public ResponseEntity<String> addQuestion(@PathVariable long courseId, @RequestBody QuestionRequest question) {
+            Optional<Course> course= courseRepository.findById(courseId);
+            if (course.isEmpty()) {
+                return ResponseEntity.status(404).body("Course not found");
+            }
+            questionBankService.addQuestion(course.get(),question);
+            return ResponseEntity.ok("Question added");
+    }
+
+    @DeleteMapping("/{courseId}/Question/{questionId}")
+    @PreAuthorize("hasRole('ROLE_INSTRUCTOR')")
+    public ResponseEntity<String> deleteQuestion(@PathVariable long courseId, @PathVariable long questionId) {
+        Optional<Course> course= courseRepository.findById(courseId);
+        if (course.isEmpty()) {
+            return ResponseEntity.status(404).body("Course not found");
+        }
+        Optional<Question> question=questionRepository.findById(questionId);
+        if (question.isEmpty()) {
+            return ResponseEntity.status(404).body("Question not found.");
+        }
+        if(question.get().getCourse().getId() != courseId){
+            return ResponseEntity.status(404).body("This question doesn't belong to this course");
+        }
+        questionBankService.deleteQuestion(course.get(),question.get());
+        return ResponseEntity.ok("Question removed");
+    }
+
+    @GetMapping("/{courseId}/getquestions")
+    @PreAuthorize("hasRole('ROLE_INSTRUCTOR') or hasRole('ROLE_STUDENT')")
+    public ResponseEntity<?> getQuestionsByCourse(@PathVariable long courseId){
+        Optional<Course> course= courseRepository.findById(courseId);
+        if (course.isEmpty()) {
+            return ResponseEntity.status(404).body("Course not found");
+        }
+        QuestionBank questionBank=course.get().getQuestionBank();
+        if(questionBank==null){
+            return ResponseEntity.status(404).body("No question bank for this course");
+
+        }
+        List<QuestionDto> questionDtos = questionBank.getQuestions()
+                .stream()
+                .map(QuestionMapper::toDTO)
+                .toList();
+        return ResponseEntity.ok(questionDtos);
+    }
 }
 
